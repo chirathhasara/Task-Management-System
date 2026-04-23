1eed7a5b28dd34fb0186807c00d31262ad49e560
chirath <chirathhasara2001@gmal.com>
2026-04-23 08:44:38 +0530

    set new queue to test

---
+++ app/Jobs/QueueHealthCheckJob.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace App\Jobs;
+
+use Illuminate\Contracts\Queue\ShouldQueue;
+use Illuminate\Foundation\Queue\Queueable;
+use Illuminate\Support\Facades\Log;
+
+class QueueHealthCheckJob implements ShouldQueue
+{
+    use Queueable;
+
+    /**
+     * Create a new job instance.
+     */
+    public function __construct()
+    {
+        //
+    }
+
+    /**
+     * Execute the job.
+     */
+    public function handle(): void
+    {
+        Log::info('QueueHealthCheckJob processed', [
+            'processed_at' => now()->toDateTimeString(),
+            'unix_time' => now()->timestamp,
+        ]);
+    }
+}
